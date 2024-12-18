84ca399871ca180a8de0db625cb17054d821e1f2
Agustín Ceballos <aceballos1923@gmail.com>
2024-12-18 15:58:06 -0300

    initial commit

---
+++ src/main/java/com/aceballos/reservas_canchas/ReservasCanchasApplication.java
@@ -0,0 +1,13 @@
+package com.aceballos.reservas_canchas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasCanchasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasCanchasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceballos/reservas_canchas/ReservasCanchasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceballos.reservas_canchas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasCanchasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
