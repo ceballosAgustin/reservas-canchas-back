8868e3fe340c36f541391b80e17f32b373b74a2a
Agustín Ceballos <aceballos1923@gmail.com>
2025-01-03 04:55:34 -0300

    update: Cambios en el servicio

--- src/main/java/com/aceballos/reservas_canchas/services/impl/AuthService.java
+++ src/main/java/com/aceballos/reservas_canchas/services/impl/AuthService.java
@@ -1,8 +1,8 @@
 package com.aceballos.reservas_canchas.services.impl;
 
-import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,14 +14,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import com.aceballos.reservas_canchas.dtos.LoginDto;
+import com.aceballos.reservas_canchas.dtos.UsuarioDto;
 import com.aceballos.reservas_canchas.entities.Usuario;
 import com.aceballos.reservas_canchas.exceptions.CredencialesInvalidasException;
 import com.aceballos.reservas_canchas.exceptions.YaExisteException;
 import com.aceballos.reservas_canchas.repositories.IUsuarioRepository;
 import com.aceballos.reservas_canchas.services.IAuthService;
 import com.aceballos.reservas_canchas.services.IUsuarioService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -49,26 +48,21 @@ public class AuthService implements IAuthService{
 
         User usuario = (User) authentication.getPrincipal();
         String email = usuario.getUsername();
-        Usuario usuarioDb = usuarioRepository.findByEmail(email).get();
+        UsuarioDto usuarioDb = usuarioService.findByEmail(email);
         String nombre = "%s %s".formatted(usuarioDb.getNombre(), usuarioDb.getApellido());
 
-        Collection<? extends GrantedAuthority> roles = usuario.getAuthorities();
-
-        Claims claims;
-        try {
-            claims = Jwts.claims()
-                            .add("authorities", new ObjectMapper().writeValueAsString(roles))
-                            .add("email", email)
-                            .add("name", nombre)
-                            .build();
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error processing JSON", e);
-        }
+        Claims claims = Jwts.claims()
+                        .add("authorities", usuario.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                        .add("email", email)
+                        .add("nombre", nombre)
+                        .build();
 
         String token = Jwts.builder()
                             .subject(email)
                             .claims(claims)
                             .expiration(new Date(System.currentTimeMillis() + 3600000))
+                            .issuedAt(new Date())
                             .signWith(SECRET_KEY)
                             .compact();
 
@@ -79,7 +73,7 @@ public class AuthService implements IAuthService{
             "mensaje", "Has iniciado sesión con éxito"
             );
         } catch (BadCredentialsException e) {
-            throw new CredencialesInvalidasException();
+            throw new CredencialesInvalidasException("Error de autenticación: email o clave incorrectos");
         }
     }
 
